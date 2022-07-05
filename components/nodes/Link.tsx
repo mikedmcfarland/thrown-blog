07235386da1aa9623605385723985b2bb153e47f
Michael McFarland <mikedmcfarland@gmail.com>
2022-07-05 12:54:35 -0600

    add missing link component and org docs

---
+++ components/nodes/Link.tsx
@@ -0,0 +1,19 @@
+
+import { Link } from "@chakra-ui/react"
+import { default as NextLink } from 'next/link'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { LinkNode } from "src/org/types"
+import { NodeComponent } from "./types"
+
+export const OrgLink: NodeComponent<LinkNode> = ({ node, children }) => {
+    const href = `${node.properties.type}:${node.properties.path}`
+    const isExternal = !node.properties["is-internal"]
+
+    return (
+        <NextLink href={href} passHref>
+            <Link isExternal={isExternal}>
+                {children} <ExternalLinkIcon mx='2px' />
+            </Link>
+        </NextLink>
+    )
+}
