9478d598e9c68db49ce9c6310727297057c3fb26
Michael McFarland <mikedmcfarland@gmail.com>
2022-07-05 12:54:16 -0600

    add more theme colors (brand orange)

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,47 @@
-import { extendTheme } from '@chakra-ui/react'
+import { mode } from '@chakra-ui/theme-tools';
 
+import { extendTheme, ThemeConfig, withDefaultColorScheme } from '@chakra-ui/react'
+
+const styles = {
+    global: props => ({
+        body: {
+            color: mode('gray.800', 'whiteAlpha.900')(props),
+            bg: mode('gray.100', '#141214')(props),
+        },
+    }),
+};
 const colors = {
-    brand: {
-        900: '#1a365d',
-        800: '#153e75',
-        700: '#2a69ac',
-    },
+    brand:
+    {
+        50: '#ffeede',
+        100: '#fcd2b5',
+        200: '#f5b589',
+        300: '#ef995c',
+        400: '#e97c2f',
+        500: '#d06216',
+        600: '#a34d0f',
+        700: '#753609',
+        800: '#472001',
+        900: '#1e0800',
+    }
+
 }
 
-export const theme =  extendTheme({ colors })
+const scheme = withDefaultColorScheme({
+    colorScheme: "brand"
+})
+
+export const config: ThemeConfig = {
+    initialColorMode: "light",
+    useSystemColorMode: false
+}
+
+
+export const theme = extendTheme(
+    {
+        config,
+        colors,
+        styles
+    }
+    , scheme
+)
