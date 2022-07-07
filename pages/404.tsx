c6cfee7bc27ed59a8e036c416989175cf398c77f
Michael McFarland <mikedmcfarland@gmail.com>
2022-07-06 22:47:03 -0600

    add custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,35 @@
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import Link from 'next/link';
+
+export default function NotFound() {
+    return (
+
+        <Box textAlign="center" py={10} px={6}>
+            <Heading
+                display="inline-block"
+                as="h2"
+                size="2xl"
+                bgGradient="linear(to-r, brand.400, brand.600)"
+                backgroundClip="text">
+                404
+            </Heading>
+            <Text fontSize="18px" mt={3} mb={2}>
+                Page Not Found
+            </Text>
+            <Text color={'gray.500'} mb={6}>
+                The page you're looking for does not seem to exist
+            </Text>
+
+            <Link href="/" passHref>
+                <Button
+                    as="a"
+                    bgGradient="linear(to-r, brand.400, brand.500, brand.600)"
+                    color="white"
+                    variant="solid">
+                    Go to Home
+                </Button>
+            </Link>
+        </Box>
+
+    );
+}
