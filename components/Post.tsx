374bdae38fbf86f5501e9f5f94efbc309630371b
Michael McFarland <mikedmcfarland@gmail.com>
2022-07-17 14:14:43 -0600

    feature: Have the Post page have a hero

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,26 +1,73 @@
-/* import Image from 'next/image'; */
-import { Center, Container, Heading } from '@chakra-ui/react'
+import {
+  Box,
+  Center,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  useColorModeValue,
+  VStack,
+} from '@chakra-ui/react'
+import HeroImage from './HeroImage'
+import Date from './Date'
+import PostTag from './PostTag'
+import { FaTag, FaTags } from 'react-icons/fa'
 type Props = {
-  summary: {
-    title: string
-  }
+  summary: Summary
   children: JSX.Element | JSX.Element[]
 }
-export function Post(props: Props) {
+
+type Summary = {
+  category: string
+  title: string
+  image?: string
+  date?: string
+  tags: string[]
+}
+
+export function Post({ summary, children }: Props) {
+  const { title, date, tags } = summary
+
   return (
     <Container maxWidth="4xl">
-      <Center>
-        <Heading
-          mb={10}
-          mt={10}
-          textColor="brand.400"
-          fontWeight={800}
-          size="xl"
-        >
-          {props.summary.title}
-        </Heading>
-      </Center>
-      {props.children}
+      <Box
+        w={'full'}
+        bg={useColorModeValue('gray.100', 'gray.700')}
+        rounded={'md'}
+        overflow={'hidden'}
+      >
+        <Center>
+          <VStack mb={10} mt={10}>
+            <HStack>
+              <VStack>
+                <Heading
+                  textColor="brand.400"
+                  textAlign={'center'}
+                  fontWeight={800}
+                  size="xl"
+                  mb={[3, 7]}
+                >
+                  {title}
+                </Heading>
+
+                {tags.length > 0 && (
+                  <HStack>
+                    {tags.length > 1 ? <FaTags /> : <FaTag />}
+                    {tags.map((tag, i) => (
+                      <PostTag key={i} tag={tag} />
+                    ))}
+                  </HStack>
+                )}
+
+                {date && <Date date={date} />}
+              </VStack>
+              <HeroImage {...summary} />
+            </HStack>
+          </VStack>
+        </Center>
+      </Box>
+
+      {children}
     </Container>
   )
 }
